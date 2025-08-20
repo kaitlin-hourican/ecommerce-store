632e89f8882bfa96599476a69e024a4c9e990ae0
Kaitlin Hourican <kaitlinhourican@outlook.com>
2025-08-20 20:26:52 +0100

    feat: Implement all core API endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json()) // allows you to parse JSON data in req.body
 
+app.use("/api/products", productRoutes);
 
-app.get("/api/products", (req, res) => {
-    res.send("yooooo wassup");
-});
-app.post("/api/products", async (req, res) => {
-    const product = req.body;
-
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: "Please provide all fields" });
-    }
-
-    const newProduct = new Product(product);
-
-    try { 
-        await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-        console.error("Error in Create Product: ", error.message);
-        res.status(500).json({ success: false, message: "Server Error" });
-    }
-  
-});
-
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:3000");
+    console.log("Server started at http://localhost:" + PORT);
 });
---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,13 @@
+const CreatePage = () => {
+    const [newProduct, setNewProduct] = useState({
+        name: "",
+        price: "",
+        image: "",
+    })
+
+    return (
+        <div>CreatePage</div>
+    )
+};
+
+export default CreatePage;
